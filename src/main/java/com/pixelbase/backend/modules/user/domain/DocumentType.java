4997979b0a69dc14cfd33e6fa5ba685eaa36b194
jpablo <juanpbdev@gmail.com>
2026-04-21 03:02:41 -0500

    feat: añadido entidad, repositorio y servicio de User

---
+++ src/main/java/com/pixelbase/backend/modules/user/domain/DocumentType.java
@@ -0,0 +1,5 @@
+package com.pixelbase.backend.modules.user.domain;
+
+public enum DocumentType {
+    DNI, RUC, CE
+}
---
+++ src/main/java/com/pixelbase/backend/modules/user/domain/Role.java
@@ -0,0 +1,5 @@
+package com.pixelbase.backend.modules.user.domain;
+
+public enum Role {
+    ADMIN, CUSTOMER
+}
