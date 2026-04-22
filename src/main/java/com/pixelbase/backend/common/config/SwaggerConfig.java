325a3db110ff43f8ba1bf87f511307d3c8b92d6d
jpablo <juanpbdev@gmail.com>
2026-04-22 14:21:12 -0500

    refactor: estandarizando visibilidad en SecurityConfig.java

--- src/main/java/com/pixelbase/backend/common/config/SwaggerConfig.java
+++ src/main/java/com/pixelbase/backend/common/config/SwaggerConfig.java
@@ -3,7 +3,6 @@ package com.pixelbase.backend.common.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,7 +18,6 @@ public class SwaggerConfig {
                         .title("PixelBase API")
                         .version("1.0")
                         .description("Documentación de la API para el E-commerce PixelBase"))
-                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
                 .components(new Components()
                         .addSecuritySchemes(securitySchemeName,
                                 new SecurityScheme()
